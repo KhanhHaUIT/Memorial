ad1e8187d784b743a3a20f8c25984c3baea8b35e
KhanhHaUIT <18520692@gm.uit.edu.vn>
2021-11-23 20:43:38 +0700

    init backend

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://test:kpcl8ihvWZHgCqzU@cluster0.rrcyu.mongodb.net/test?authSource=admin&replicaSet=atlas-u3bbz4-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
+
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
