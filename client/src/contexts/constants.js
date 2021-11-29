2143db292daa4b7d127d7d91cf4ba27e0eda30ce
KhanhHaUIT <18520692@gm.uit.edu.vn>
2021-11-29 11:59:08 +0700

    Update api url for production

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,7 +1,7 @@
 export const apiUrl =
 	process.env.NODE_ENV !== 'production'
 		? 'http://localhost:4000/api'
-		: 'https://sleepy-inlet-56101.herokuapp.com/api'
+		: 'https://anything-gg.herokuapp.com/api'
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'token'
 
